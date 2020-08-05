52a2293605f32174a1dc25ce57dabbc6b4d02e6b
yezii <1126yezy@gmail.com>
2020-08-05 21:45:45 +0900

    Controlling brush size and Filling mode in paint board
    Brush size, Filling Mode

--- app.js
+++ app.js
@@ -1,14 +1,21 @@
 const canvas = document.getElementById("jsCanvas");
 const ctx = canvas.getContext("2d"); // Context of canvas is what controls pixels inside of it
 const colors = document.getElementsByClassName("jsColor");
+const range = document.getElementById("jsRange");
+const mode = document.getElementById("jsMode");
 
-canvas.width = 700;
-canvas.height = 700; // Canvas should have css size and pixel modifier size (its width and height)
+const INITIAL_COLOR = "#2c2c2c";
+const CANVAS_SIZE = 700; // Canvas should have css size and pixel modifier size
 
-ctx.strokeStyle = "#2c2c2c"; // All the lines inside context have color
+canvas.width = CANVAS_SIZE;
+canvas.height = CANVAS_SIZE;
+
+ctx.strokeStyle = INITIAL_COLOR; // All the lines inside context have color
+ctx.fillStyle = INITIAL_COLOR;
 ctx.lineWidth = 2.5; // The width of line is 2.5px
 
 let painting = false;
+let filling = false;
 
 function stopPainting() {
   painting = false;
@@ -34,6 +41,28 @@ function onMouseMove(event) {
 function handleColorClick(event) {
   const color = event.target.style.backgroundColor;
   ctx.strokeStyle = color; // strokeStyle is gonna be the color on the target
+  ctx.fillStyle = color;
+}
+
+function handleRangeChange(event) {
+  const size = event.target.value;
+  ctx.lineWidth = size;
+}
+
+function handleModeClick(event) {
+  if (filling === true) {
+    filling = false;
+    mode.innerText = "Fill";
+  } else {
+    filling = true;
+    mode.innerText = "Paint";
+  }
+}
+
+function handleFillClick() {
+  if (filling) {
+    ctx.fillRect(0, 0, canvas.width, canvas.height);
+  }
 }
 
 if (canvas) {
@@ -41,8 +70,17 @@ if (canvas) {
   canvas.addEventListener("mousedown", startPainting); // Click on mouse
   canvas.addEventListener("mouseup", stopPainting);
   canvas.addEventListener("mouseleave", stopPainting);
+  canvas.addEventListener("click", handleFillClick);
 }
 
 Array.from(colors).forEach((color) =>
   color.addEventListener("click", handleColorClick)
 );
+
+if (range) {
+  range.addEventListener("input", handleRangeChange);
+}
+
+if (mode) {
+  mode.addEventListener("click", handleModeClick);
+}
